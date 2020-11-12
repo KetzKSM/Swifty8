02de67158f9c23182be4d8760d40f0df4a5fa471
Kyle Metz <17kylemetz@gmail.com>
2020-11-11 21:51:05 -0500

    Initialization

---
+++ Swifty8/Swifty8/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Swifty8
+//
+//  Created by Kyle Metz on 11/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swifty8/Swifty8Tests/Swifty8Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Swifty8Tests.swift
+//  Swifty8Tests
+//
+//  Created by Kyle Metz on 11/11/20.
+//
+
+import XCTest
+@testable import Swifty8
+
+class Swifty8Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
